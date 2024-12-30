11160f46cff395d0ac88917c7440a1e10543c5ea
Christoph Michelbach <michelbach94@gmail.com>
2024-12-30 15:33:29 +0100

    Implement MapWithErrorParallel().

--- map.go
+++ map.go
@@ -1,5 +1,7 @@
 package fp
 
+import "sync"
+
 // Calls a defined callback function on each element of an array, and returns an array that contains the results.
 func Map[T any, R any](callback func(T) R) func([]T) []R {
 	return func(xs []T) []R {
@@ -57,5 +59,45 @@ func MapWithSlice[T any, R any](callback func(T, int, []T) R) func([]T) []R {
 		}
 
 		return result
+	}
+}
+
+func MapWithErrorParallel[T any, R any](callback func(T) (R, error)) func([]T) ([]R, error) {
+	return func(xs []T) ([]R, error) {
+		type result struct {
+			value R
+			err   error
+			index int
+		}
+
+		results := make(chan result, len(xs))
+		var wg sync.WaitGroup
+
+		// Start goroutines for parallel execution.
+		for i, x := range xs {
+			wg.Add(1)
+			go func(i int, x T) {
+				defer wg.Done()
+				res, err := callback(x)
+				results <- result{value: res, err: err, index: i}
+			}(i, x)
+		}
+
+		// Close the results channel when all goroutines complete.
+		go func() {
+			wg.Wait()
+			close(results)
+		}()
+
+		// Collect results.
+		finalResults := make([]R, len(xs))
+		for r := range results {
+			if r.err != nil {
+				return nil, r.err
+			}
+			finalResults[r.index] = r.value
+		}
+
+		return finalResults, nil
 	}
 }
